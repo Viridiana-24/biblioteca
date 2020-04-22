d0385d6e5dd293c7844152daf669aef896197bc0
Viridiana-24 <viridiana_rdz24@hotmail.com>
2020-04-22 02:48:38 -0500

    Commit inicial 2

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -22,7 +22,7 @@ app.get('/usuario', [verificaToken],  (req, res) => {
         });
 });
 
-app.post('/usuario', [verificaToken], (req, res) => {
+app.post('/usuario', (req, res) => {
     let body = req.body;
 
     let usuario = new Usuario({
